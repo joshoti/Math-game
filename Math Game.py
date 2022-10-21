2ced0e5b8eefbb36fa9611a7d032c1a46f133d9a
Joshua Oti <joshoti539@gmail.com>
2022-10-21 15:38:20 +0100

    Cleaned
    Replaced calculation if block with eval(...)

--- Math Game.py
+++ Math Game.py
@@ -1,64 +1,79 @@
 '''Math game'''
-def que():
-    from random import choice as rc
-    ops = ['+','-','*','//','%','**']
-    num = [i for i in range(101)]
-    snum = [i for i in range(21)]
-    q = f"{str(rc(num))} {rc(ops)} {str(rc(num))}"
-    check = q.split()
-    if check[1] == '*': check[2] = str(int(check[2])%10); return ' '.join(check) # If * operator, Make the RHS between 0-10
-    elif check[1] == '%': check[2] = str(rc([2,3,5,10])); return ' '.join(check) # If % operator, Make RHS either 2,3,5,10
-    elif check[1] == '**': check[2] = str(rc([0,1,2])); check[0] = str(rc(snum)); return ' '.join(check) # If ** operator, Make LHS between 0-20 and RHS between 0-2
-    return q
+from random import choice, randint
+def questionGenerator():
+    """
+    Variables
+        leftHand(str)
+        rightHand(str)
+        operators(list): [add, subtract, multiply, floor division, modulo, power]
+            
+    Returns(str)
+        space separated equation. Eg "4 ** 1"
+    """    
+    operators = ['+', '-', '*', '//', '%', '**']
+    leftHand = str(randint(0,100))
+    operator = choice(operators)
+    rightHand = randint(0,100)    
+    if operator == '*':
+        rightHand = str(rightHand%10) # If * operator, Make the RHS between 0-10
+    elif operator == '%':
+        rightHand = str(choice([2,3,5,10])) # If % operator, Make RHS either 2,3,5,10
+    elif operator == '**':
+        rightHand = str(choice([0,1,2]))
+        leftHand = str(randint(0, 20)) # If ** operator, Make LHS between 0-20 and RHS between 0-2
+    return f"{leftHand} {operator} {str(rightHand)}"
 
 def run():
-    count = 0
-    for _ in range(10):
-        print(f"Q. {_+1} / 10")
-        q = que() # Get question from question function
+    correctCount = 0
+    for i in range(10):
+        print(f"Question {i+1} / 10")
+        question = questionGenerator() # Get question from question function
         try:
-            x = int(input(f"\t\t{q} ")) # Your answer
-        except:
+            userAnswer = int(input(f"\t\t{question}  ")) # Your answer
+        except ValueError:
             print('Input not recognised')
             continue
-        part = q.split()
-        if part[1]=='+': y = int(part[0]) + int(part[2]) # IF Block computes correct answer
-        elif part[1]=='-': y = int(part[0]) - int(part[2])
-        elif part[1]=='*': y = int(part[0]) * int(part[2])
-        elif part[1]=='//': y = int(part[0]) // int(part[2])
-        elif part[1]=='%': y = int(part[0]) % int(part[2])
-        elif part[1]=='**': y = int(part[0]) ** int(part[2])
-        print(y) # Outputs correct answer
-        if x==y: print('Correct!\n'); count +=1
-        elif x!=y: print('Incorrect\n')
-    print(count,'Correct answer(s) out of 10')
-    if 8<=count: print('Excellent Performance!')
-    elif 6<=count<8: print('Above Average Performance!')
-    elif 4<=count<6: print('Average Performance!')
-    elif count<4: print('You can do better Kido!')
-    return count
+        
+        correctAnswer = eval(question) # Computes string to get correct answer
+        if userAnswer == correctAnswer:
+            print('Correct!\n')
+            correctCount +=1
+        elif userAnswer != correctAnswer:
+            print(f"Incorrect\nCorrect answer is {correctAnswer}\n")
+            
+    print(correctCount,'Correct answer(s) out of 10')
+    if 8 <= correctCount: print('Excellent Performance!\n')
+    elif 6 <= correctCount < 8: print('Above Average Performance!\n')
+    elif 4 <= correctCount < 6: print('Average Performance!\n')
+    elif correctCount < 4: print('You can do better Kido!\n')
+    return correctCount
 
 def hint():
-    print("HINT:")
-    print('// means floor division. Return the integer part of division. Eg:\n5 // 2 = 2\t(2 rem 1)\n8 // 3 = 2\t(2 rem 2)\n4 // 7 = 0\t(0 rem 7)\n')
-    print('% means modulus. Return the remainder after dividing. Eg:\n5 % 2 = 1\t(2 rem 1)\n9 % 3 = 0\t(3 rem 0)\n4 % 7 = 4\t(0 rem 7)\n')
+    print("\n=================  HINT  =================\n")
+    print('// means floor division. Return the integer part of division. Eg:\n5 // 2 = 2\t(2 rem 1)\n8 // 3 = 2\t(2 rem 2)\n4 // 7 = 0\t(0 rem 4)\n')
+    print('% means modulus. Return the remainder part of division. Eg:\n5 % 2 = 1\t(2 rem 1)\n9 % 3 = 0\t(3 rem 0)\n4 % 7 = 4\t(0 rem 4)\n')
     print('** means exponent/raised to the power of. Eg:\n5 ** 2 = 25\n2 ** 3 = 8\n4 ** 1 = 4')
 
 hint()
-progress = {} # To save the score of all your tries
+progress = [] # To save the score of all your tries
 tries = 0
 retry = 'y'
-while retry!='n':
-    tries+=1
-    if tries>1:
-        print('')
-        for k,v in progress.items(): print(f"Try {k}: {v}") # Prints score of all your tries
-    print('\nBegin!\n')
-    progress[tries] = run() # This lines saves the score in dictionary
+while retry != 'n':
+    tries += 1
+    if tries > 1:
+        print()
+        for attemptNo, score in enumerate(progress, start=1):
+            print(f"Try {attemptNo}: {score}") # Prints score of all your tries
+    print('\n=================  Begin!  =================\n')
+    progress.append(run()) # This lines saves the score in list
     while True:
         retry = input("Would you like to try again. ( y/n ) ")
-        if (retry == 'y') | (retry == 'n'): break
-    if retry == 'y': hints = input("Would you like to view the hint: ( y/n )")
-    if retry == 'n': continue
-    if hints == 'y': hint()
+        if (retry == 'y') | (retry == 'n'):
+            break
+    if retry == 'y':
+        hints = input("Would you like to view the hint: ( y/n ) ")
+    elif retry == 'n':
+        continue
+    if hints == 'y':
+        hint()
     
